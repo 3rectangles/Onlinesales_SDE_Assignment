3354303466fe45356798ef57aec72365d30080f9
3rectangles <kashish242pandit@gmail.com>
2023-10-18 00:54:24 +0530

    changed log

--- Task2_MathApi/Main.java
+++ Task2_MathApi/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Solve Maths expressions using 3rd party API mathorg!");
     }
 }
